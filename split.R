ca976955becfa4fda0f16176d84f91c07974a17a
yukiecho <yukiecho@users.noreply.github.com>
2015-02-09 13:51:50 -0500

    Update split.R

--- split.R
+++ split.R
@@ -4,5 +4,6 @@ str(iris)
 aa a<- split(iris,iris$Species)
 ## species is a factor variable with 3 levels
 ## then aa is a list of 3, each has 5 variables
-aa[1]$setosa$Sepal.Length
+aa$setosa$Sepal.Length
 ## you have to reference the "factor" at first and then reference the specific variable
+
