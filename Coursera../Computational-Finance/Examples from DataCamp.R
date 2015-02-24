73dfb6762d7d36a38fd44b6046faca1cc25fa73e
yukiecho <yukiecho@users.noreply.github.com>
2015-02-24 11:46:50 -0500

    Update Examples from DataCamp.R

--- Coursera../Computational-Finance/Examples from DataCamp.R
+++ Coursera../Computational-Finance/Examples from DataCamp.R
@@ -49,3 +49,43 @@ sbux_ccret <- log(sbux_prices_df[2:n,1]) - log(sbux_prices_df[1:(n-1),1])
 
 # Assign names to the continuously compounded 1-month returns
 names(sbux_ccret) <- sbux_df[2:n,1]
+
+# Compare the simple and cc returns
+head(X <-  cbind(sbux_ret,sbux_ccret))
+
+# The simple returns (sbux_ret) and the continuously compounded returns (sbux_ccret) have been preloaded in your workspace
+
+# Plot the returns on the same graph
+plot(sbux_ret, type="l", col="blue", lwd=2, ylab="Return",
+               main="Monthly Returns on SBUX")
+
+# Add horizontal line at zero
+abline(h=0)
+
+# Add a legend
+legend(x="bottomright", legend=c("Simple", "CC"), 
+       lty=1, lwd=2, col=c("blue","red"))
+
+# Add the continuously compounded returns
+lines(sbux_ccret,col="red",lwd=2)
+
+# Instructions for above codes
+# Your task is to add the continuously compounded returns to the plot. 
+# This can be done with the lines() function, having sbux_ccret as a first argument. 
+# Furthermore, the line should be red and its thickness should be set to 2. 
+# You can do that by setting the col and lwd arguments.
+
+# Assign to the variable sbux_gret the gross returns (which is the simple return + 1).
+# Assign to sbux_fv a vector that contains the future values of the $1 invested in SBUX for every time period.
+# Have a look at the plot. Would it have been a good idea to invest in SBUX?
+# The simple returns (sbux_ret) and the continuously compounded returns (sbux_ccret) have been preloaded in your workspace
+
+# Compute gross returns
+sbux_gret <- 1 + sbux_ret
+
+# Compute future values
+sbux_fv <- cumprod(sbux_gret)
+
+# Plot the evolution of the $1 invested in SBUX as a function of time
+plot(sbux_fv, type="l", col="blue", lwd=2, ylab="Dollars", 
+     main="FV of $1 invested in SBUX")
