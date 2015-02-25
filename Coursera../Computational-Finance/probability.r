778fd4bc098e0ab6084a7b4eff9552751beb9a26
yukiecho <yukiecho@users.noreply.github.com>
2015-02-25 09:36:09 -0500

    Update probability.r

--- Coursera../Computational-Finance/probability.r
+++ Coursera../Computational-Finance/probability.r
@@ -10,3 +10,10 @@ pnorm(-0.10, mean = mu_x, sd = sigma_x)
 
 # Pr(-0.05 < X < 0.15)
 pnorm(0.15, mean = mu_x, sd = sigma_x) - pnorm(-0.05, mean = mu_x, sd = sigma_x)
+
+# For the grid of values in x_vals, compute the values of the assumed normal density for the return on Microsoft stock and assign it to MSFT.
+# For the grid of values in x_vals, compute the values of the assumed normal density for the return on Starbucks stock and assign it to SBUX
+# Normally distributed monthly returns
+x_vals <- seq(-0.25, 0.35, length.out = 100)
+MSFT <- dnorm(x_vals, mean = 0.05, sd = 0.1)
+SBUX <- dnorm(x_vals, mean = 0.025, sd = 0.05)
