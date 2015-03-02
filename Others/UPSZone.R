f3cda6baa82e75c343ba325ec968a24ad84078df
yukiecho <yukiecho@users.noreply.github.com>
2015-03-02 11:45:50 -0500

    Update and rename Others to Others/UPSZone.R

---
+++ Others/UPSZone.R
@@ -0,0 +1,20 @@
+library(xlsx)
+list <- read.xlsx("upsrate.xls",1)
+list_df <- data.frame(list)
+final <- read.xlsx("upsrate.xls",2)
+final_df <- data.frame(final)
+a <- rep(1,417)
+final_df[,"zone"] <- a
+
+for(i in 1:nrow(final_df)){
+  interval <- final_df[i,1]-4
+  print(i)
+  for (j in 1:nrow(list_df)){
+    if(list_df[j,3]>=interval){
+      final_df[i,2] <- list_df[j,4]
+      break    
+    }   
+  }
+}
+
+write.csv(final_df, "zone.csv")
